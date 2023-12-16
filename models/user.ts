746380b961f68cda856f45051f3a479a10216767
DESKTOP-8N1CV5H\WRH <scps970412@gmail.com>
2023-12-16 15:10:56 +0800

    [dev] 加入User新增功能

--- models/user.ts
+++ models/user.ts
@@ -1,6 +1,13 @@
 export class User {
-  Id: number;
-  UserId: string;
-  Email: string;
-  Password: string;
+  id: number;
+  userId: string;
+  email: string;
+  password: string;
+
+  constructor() {
+    this.id = 0;
+    this.userId = "";
+    this.email = "";
+    this.password = "";
+  }
 }
