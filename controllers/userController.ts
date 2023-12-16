2fd23145f3c392d33d0ab52bfc99409e5840091e
DESKTOP-8N1CV5H\WRH <scps970412@gmail.com>
2023-12-16 16:12:59 +0800

    [dev] 新增 Update 功能

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,6 +1,7 @@
 import { User } from "./../models/user";
 import { Request, Response } from "express";
 import UserService from "../service/userService";
+import userService from "../service/userService";
 
 var express = require("express");
 var router = express.Router();
@@ -9,6 +10,13 @@ router.post("/add", function (req: Request, res: Response, next: any) {
   let user: User = req.body;
   let isAdd: boolean = UserService.add(user);
   res.json(isAdd);
+  next();
+});
+
+router.put("/update", function (req: Request, res: Response, next: any) {
+  let user: User = req.body;
+  let isUpdate: boolean = userService.update(user);
+  res.json(isUpdate);
   next();
 });
 
--- service/userService.ts
+++ service/userService.ts
@@ -19,6 +19,24 @@ class UserService {
       });
     return false;
   }
+
+  update(user: User): boolean {
+    const updateUser = new PQ(
+      `UPDATE public.user SET userid=$2, password=$3, email=$4
+       WHERE id = $1`
+    );
+    updateUser.values = [user.id, user.userId, user.password, user.email];
+
+    db.none(updateUser)
+      .then(() => {
+        return true;
+      })
+      .catch((error: Error) => {
+        //加入log檔
+        console.log(error);
+      });
+    return false;
+  }
 }
 
 export default new UserService();
