2ccef167b5534bba075d26efa89bc405c6d199ab
DESKTOP-8N1CV5H\WRH <scps970412@gmail.com>
2023-12-24 18:03:56 +0800

    [dev] 新增新增修改刪除

--- controllers/photoController.ts
+++ controllers/photoController.ts
@@ -1,7 +1,9 @@
 import File from "../tools";
 import { cwd } from "node:process";
 import PhotoService from "../service/photoService";
-import { Photo } from "../models/photo";
+import AlbumService from "../service/albumService";
+import Photo from "../models/photo";
+import Album from "../models/album";
 
 var express = require("express");
 var router = express.Router();
@@ -9,30 +11,122 @@ const path = require("path");
 const multer = require("multer");
 const upload = multer({ dest: "tmp_uploads/" });
 
-router.post("/add", upload.array("files", 2), async (req: any, res: any) => {
+router.post("/add", upload.single("file"), async (req: any, res: any) => {
   let reuslt = {
-    success: false,
+    success: true,
     message: "",
   };
 
-  let user = req.session.user;
-  let isExis: boolean = false;
   const file = req.file;
-  const userPath = path.join(cwd(), "user", user.account, "測試2");
+  let photo: Photo = {
+    id: 0,
+    albumid: Number(req.body.albumid),
+    filename: file.originalname,
+    description: req.body.description,
+  };
+
+  let user = req.session.user;
+  let album: Album = await AlbumService.getById(photo.albumid);
+  const userPath = path.join(cwd(), "user", user.account, album.title);
   const newFilePath = path.join(userPath, file.originalname);
-  if (File.isExist(newFilePath)) {
-    isExis = true;
+  let isExis: boolean = File.isExist(newFilePath);
+  if (isExis) {
     reuslt.success = false;
     reuslt.message = "檔案名稱已重複";
+  } else {
+    let isAdd: boolean = await PhotoService.add(photo);
+    if (isAdd) {
+      const filePath = path.join(cwd(), file.path);
+      const newFilePath = path.join(userPath, file.originalname);
+      File.copyFile(filePath, newFilePath);
+      reuslt.message = "新增成功";
+    } else {
+      reuslt.message = "新增失敗";
+    }
   }
+  File.deleteFile(cwd(), file.path);
 
-  if (!isExis) {
-    const filePath = path.join(cwd(), file.path);
-    const newFilePath = path.join(userPath, file.originalname);
-    File.copyFile(filePath, newFilePath);
-    File.deleteFile(cwd(), file.path);
+  res.status(200).json(reuslt);
+});
+
+router.post(
+  "/update",
+  upload.single("file"),
+  async function (req: any, res: any) {
+    let reuslt = {
+      success: false,
+      message: "",
+    };
+
+    const file = req.file;
+    let photo: Photo = {
+      id: Number(req.body.id),
+      albumid: Number(req.body.albumid),
+      filename: file == undefined ? "" : file.originalname,
+      description: req.body.description,
+    };
+    let album: Album = await AlbumService.getById(photo.albumid);
+    const userPath = path.join(
+      cwd(),
+      "user",
+      req.session.user.account,
+      album.title
+    );
+
+    let isExis: boolean = true;
+    let dbPhoto: Photo = await PhotoService.getById(photo.id);
+    if (photo.filename == "") {
+      photo.filename = dbPhoto.filename;
+    } else {
+      const newFile = path.join(userPath, file.originalname);
+      isExis = File.isExist(newFile);
+      if (isExis) {
+        reuslt.success = false;
+        reuslt.message = "檔案名稱已重複";
+      }
+    }
+
+    if (file != undefined) {
+      const newFile = path.join(userPath, file.originalname);
+      File.deleteFile(userPath, dbPhoto.filename);
+      const tempPath = path.join(cwd(), file.path);
+      File.copyFile(tempPath, newFile);
+    }
+
+    let isUpdate = await PhotoService.update(photo);
+    if (isUpdate) {
+      reuslt.message = "修改成功";
+    } else {
+      reuslt.message = "修改失敗";
+    }
+
+    if (file != undefined) {
+      File.deleteFile(cwd(), file.path);
+    }
+
+    res.status(200).json(reuslt);
   }
+);
 
+router.post("/delete", async (req: any, res: any) => {
+  let reuslt = {
+    success: true,
+    message: "",
+  };
+  let id = Number(req.body.id);
+  let photo: Photo = await PhotoService.getById(id);
+  let album: Album = await AlbumService.getById(photo.albumid);
+
+  let isDelect = await PhotoService.delete(photo);
+  if (isDelect) {
+    let user = req.session.user;
+    let fliePath = path.join(cwd(), "user", user.account, album.title);
+    File.deleteFile(fliePath, photo.filename);
+    reuslt.message = "刪除成功";
+  } else {
+    reuslt.success = false;
+    reuslt.message = "刪除失敗";
+  }
   res.status(200).json(reuslt);
 });
 
